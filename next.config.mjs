c2410777763229f94171e5cbac46b6bd2638b7e9
yassine <yacineaoufi743@gmail.com>
2024-06-20 18:31:15 +0100

    fcf

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,21 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: [
-'localhost',
-'degitalmerketex-production-2851.up.railway.app/',
-      ],
-    }, 
-  };
-   
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '', // specify the port if needed, otherwise leave it empty
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'degitalmerketex-production-2851.up.railway.app',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+export default nextConfig;
